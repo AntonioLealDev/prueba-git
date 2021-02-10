a3f2d45418e7c01e2d846ca6cb6f62a0f966d97a
AntonioLealDev <antonio.leal.dev@gmail.com>
2021-02-10 15:49:36 +0100

    last contents to match original repo

--- api.py
+++ api.py
@@ -1,13 +1,13 @@
 from​ flask ​import​ Flask, render_template
 from​ src ​import​ covid_dash, hospitals_tb
+print("\nData found, your API's working!!!")
+print()
 
 app = Flask(__name__)
+
 @app.route(​"/"​)
 def​ landing_page():​
     return​ render_template(​'index.html'​)
-
-if​ __name__ == ​'__main__'​:
-    app.run(host=​'0.0.0.0'​,port=​1991​, debug=​True​)
 
 @app.route(​"/dashboard"​)
 def​ dashboard():
@@ -16,3 +16,7 @@ def​ dashboard():
 @app.route(​"/map"​)
 def​ map():
     ​return​ render_template(​'map.html'​)
+    
+
+if​ __name__ == ​'__main__'​:
+    app.run(host=​'0.0.0.0'​,port=​1991​, debug=​True​)
